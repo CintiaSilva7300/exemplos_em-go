f5178b1be512c0e0290d26f4cf5e8953bed1aa2e
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-03-24 12:53:45 -0300

    manipulando aquivos e lendo string por bytes

--- manipulandoArquivo/main.go
+++ manipulandoArquivo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -17,8 +18,31 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Arquivo criado com sucesso! Tamanho: %d bytes", tamanho)
+	fmt.Printf("Arquivo criado com sucesso! Tamanho: %d bytes\n", tamanho)
+	defer f.Close()
 	
 
-	defer f.Close()
+	//leitura de arquivo
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(arquivo)) //converte para string "slice de bytes"
+
+	//leitura de pouco em pouco abrindo o aquivo
+	arquivo2, err := os.Open("arquivo.txt") //abre o arquivo
+	if err != nil {
+		panic(err)
+	}
+	
+	reader := bufio.NewReader(arquivo2) //cria(reader) o leitor
+	buffer := make([]byte, 1)//cria o buffer de 10 bytes
+
+	for {
+		n, err := reader.Read(buffer)//le o buffer
+		if err != nil {
+			break
+		}
+		fmt.Println("Lido cada pedaço de 1 byte: ",string(buffer[:n])) //converte para string "slice de bytes" n é a posição do buffer
+	}
 }
